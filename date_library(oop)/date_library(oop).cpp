746fc3f0d79b08e0399ff6472bd3ee11b57fe21b
abdellatif <outegnitabdellatif@gmail.com>
2025-08-24 20:17:12 +0100

    c++ date library (class)

---
+++ date_library(oop)/date_library(oop).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "cls_date.h"
+
+using namespace std; 
+
+int main()
+{
+	cls_date Date;
+	cls_date Date1(1 , 1 , 2025);
+	cls_date Date2(250, 2022);
+
+	
+	Date1.DecreaseDateByOneDay();
+	Date1.print();
+
+	Date.increase_day();
+
+	cout << Date.DaysUntilTheEndOfWeek(Date) << endl;
+
+}
